3c85c92087fc8433706f21252e06fd69b764854c
ac1989 <axcarter89@gmail.com>
2022-12-02 17:54:15 +0000

    day 1

---
+++ 01/index.js
@@ -0,0 +1,70 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+exports.__esModule = true;
+var promises_1 = require("node:fs/promises");
+var A = require("fp-ts/Array");
+var TE = require("fp-ts/TaskEither");
+var E = require("fp-ts/Either");
+var N = require("fp-ts/number");
+var O = require("fp-ts/Option");
+var function_1 = require("fp-ts/function");
+var getInput = TE.tryCatch(function () { return (0, promises_1.readFile)("./input", "utf-8"); }, function (e) { return e; });
+var isNotEmpty = function (s) { return s !== ""; };
+var getMostCalorific = (0, function_1.flow)(A.chop(function (xs) {
+    var _a = A.spanLeft(isNotEmpty)(xs), init = _a.init, rest = _a.rest;
+    return [init, A.dropLeft(1)(rest)];
+}), A.map(A.reduce(0, function (acc, cur) { return acc + parseInt(cur); })), A.sort(N.Ord), A.last, O.getOrElse(function () { return 0; }));
+function run() {
+    return __awaiter(this, void 0, void 0, function () {
+        var _a;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
+                case 0:
+                    _a = function_1.pipe;
+                    return [4 /*yield*/, getInput()];
+                case 1:
+                    _a.apply(void 0, [_b.sent(), E.map(function (input) { return input.split("\n"); }),
+                        E.map(getMostCalorific),
+                        E.getOrElse(function () { return 0; }),
+                        console.log]);
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+run();
---
+++ 01/index.ts
@@ -0,0 +1,38 @@
+import { readFile } from "node:fs/promises";
+
+import * as A from "fp-ts/Array";
+import * as TE from "fp-ts/TaskEither";
+import * as E from "fp-ts/Either";
+import * as N from "fp-ts/number";
+import * as O from "fp-ts/Option";
+import { pipe, flow } from "fp-ts/function";
+
+const getInput = TE.tryCatch(
+  () => readFile("./input", "utf-8"),
+  (e) => e
+);
+
+const isNotEmpty = (s: string) => s !== "";
+
+const getMostCalorific = flow(
+  A.chop((xs: string[]) => {
+    const { init, rest } = A.spanLeft(isNotEmpty)(xs);
+    return [init, A.dropLeft(1)(rest)];
+  }),
+  A.map(A.reduce(0, (acc, cur) => acc + parseInt(cur))),
+  A.sort(N.Ord),
+  A.last,
+  O.getOrElse(() => 0)
+);
+
+async function run() {
+  pipe(
+    await getInput(),
+    E.map((input) => input.split("\n")),
+    E.map(getMostCalorific),
+    E.getOrElse(() => 0),
+    console.log
+  );
+}
+
+run();
